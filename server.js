2d901eabd41fb72e0b0afedf8d508848b7ab2be0
Trevor Keith <keithtw@gmatc.matc.edu>
2019-10-20 20:16:09 -0500

    Created a User Registration API Endpoint

--- server.js
+++ server.js
@@ -1,12 +1,33 @@
 import express from 'express';
 import connectDatabase from './config/db';
 
+//Initialize express application
 const app = express();
 
+// Connect database
 connectDatabase();
 
+// Configure Middleware
+app.use(express.json({extended: false}));
+
+// API Endpoints
+/*
+    @route GET /
+    @desc Test endpoint
+*/
 app.get('/', (reg, res) =>
     res.send('http get request sent to root api endpoint')
 );
 
+/*
+    @route POST api/users
+    @desc Register user
+*/
+app.post('/api/users', (req, res) => {
+    console.log(req.body);
+    res.send(req.body);
+
+});
+
+// Connection listener
 app.listen(3000, () => console.log('Express server running on port 3000'));

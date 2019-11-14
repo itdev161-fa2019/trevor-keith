430e787bc78d0ccec294dc297b27a0a5fbbc2c91
Trevor Keith <keithtw@gmatc.matc.edu>
2019-11-14 02:39:20 -0600

    Updated Register.js

--- client/src/components/Register/Register.js
+++ client/src/components/Register/Register.js
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import { useHistory } from 'react-router-dom';
 
-const Register = () => {
+const Register = ({ authenticateUser }) => {
     const [userData, setUserData] = useState({
         name : '',
         email: '',
@@ -10,7 +11,10 @@ const Register = () => {
 
     });
 
+    const[errorData, setErrorData] = useState({ errors: null });
+
     const { name, email, password, passwordConfirm } = userData;
+    const { errors } = errorData;
 
     const onChange = e => {
         const { name, value } = e.target;
@@ -22,7 +26,7 @@ const Register = () => {
 
     }
 
-    const register = async () => {
+    const registerUser = async () => {
         if (password !== passwordConfirm) {
             console.log('Passwords do not match');
 
@@ -46,14 +50,23 @@ const Register = () => {
                 const body = JSON.stringify(newUser);
                 const res = await axios.post('http://localhost:5000/api/users', body, config);
 
-                console.log(res.data);
+                // Store user data and redirect
+                localStorage.setItem('token', res.data.token);
+                history.pushState('/');
 
             } catch(error) {
-                console.error(error.response.data);
+                // Clear user data and set errors
+                localStorage.removeItem('token');
 
-                return;
+                setErrorData({
+                    ...errors,
+                    errors: error.response.data.errors
+
+                })
 
             }
+
+            authenticateUser();
 
         }
 
@@ -101,7 +114,12 @@ const Register = () => {
             </div>
 
             <div>
-                <button onClick={() => register()}>Register</button>
+                <button onClick={() => registerUser()}>Register</button>
+
+            </div>
+            <div>
+                {errors && errors.map(error =>
+                    <div key={error.msg}>{error.msg}</div>)}
 
             </div>
 
